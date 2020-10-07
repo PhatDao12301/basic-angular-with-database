0b207ddd637ce7528d98d5abb3e9333f28d27b75
PhatDao12301 <phat4everduke@gmail.com>
2020-10-07 22:47:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('./dist/basic-angular-with-database.json'));
+app.use(express.static('./dist/basic-angular-with-database'));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', {
-    root: 'dist/basic-angular-with-database.json/'
+    root: 'dist/basic-angular-with-database/'
   });
 });
 
